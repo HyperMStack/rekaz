f10aa0157fc3fbd4fdd83a3c59bd1170c57be8dc
MaenAlamin <ma3n.93@gmail.com>
2024-05-06 09:21:59 +0300

    temporarly removed the project hero position fixed

--- src/components/project/Hero.js
+++ src/components/project/Hero.js
@@ -2,8 +2,10 @@ export function Hero({ image }) {
   return (
     <>
       <div
-        className="h-128 w-full md:h-172 lg:h-196 2xl:h-256 md:bg-top bg-no-repeat bg-cover bg-bottom md:bg-fixed relative"
-        style={{ backgroundImage: `url(${image})` }}
+        className="w-full h-128 md:h-172 lg:h-196 bg-no-repeat bg-cover relative bg-center"
+        style={{
+          backgroundImage: `url(${image})`,
+        }}
       >
         <div className="absolute inset-0 bg-gradient-to-b from-[#062c3b] via-transparent to-transparent"></div>
       </div>
