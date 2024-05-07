7f1441118b3735bbe33dd51b11d1638624373957
MaenAlamin <ma3n.93@gmail.com>
2024-05-07 09:46:00 +0300

    added new images and fixed few bugs in production

--- src/pages/project.js
+++ src/pages/project.js
@@ -8,49 +8,10 @@ import { WideImage } from "../components/project/WideImage";
 import { navLinks, websiteData } from "@/data/data";
 import { ImageSlider } from "@/components/project/ImageSlider";
 import Head from "next/head";
-
-const projectData = {
-  coverImage: "/images/project1/1.webp",
-  title: "Project Title",
-  slug: "project-title",
-  keywords: "",
-
-  description:
-    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque numquam tenetur dolorem ullam dolores animi corporis illum harum. Obcaecati soluta a atque culpa ipsam necessitatibus asperiores facere modi eos mollitia. Molestiae vitae minima laudantium, sapiente nostrum ipsa, facilis, voluptates perferendis quaerat inventore quidem eligendi. A deserunt culpa necessitatibus aperiam illo commodi, dolorum similique eaque. Distinctio porro officiis ipsa? Laboriosam, alias!",
-  clientName: "Client's name",
-  sector: "Buildings",
-  location: "Muscat, Oman",
-  imageSet_1: [
-    {
-      id: 1,
-      image: "/images/project1/2.webp",
-      caption: "this is a capption for the image Image",
-    },
-    { id: 2, image: "/images/project1/3.webp", caption: "" },
-  ],
-  imageSet_2: [
-    { id: 1, image: "/images/project1/4.webp", caption: "" },
-    { id: 2, image: "/images/project1/5.webp", caption: "" },
-  ],
-  sliderImages: [
-    { id: 1, image: "/images/project1/6.webp" },
-    { id: 2, image: "/images/project1/7.webp" },
-    { id: 2, image: "/images/project1/8.webp" },
-    { id: 2, image: "/images/project1/9.webp" },
-    { id: 2, image: "/images/project1/10.webp" },
-    { id: 2, image: "/images/project1/11.webp" },
-    { id: 2, image: "/images/project1/12.webp" },
-    { id: 2, image: "/images/project1/13.webp" },
-    { id: 2, image: "/images/project1/14.webp" },
-    { id: 2, image: "/images/project1/15.webp" },
-    { id: 2, image: "/images/project1/16.webp" },
-  ],
-  wideImage: "/images/project1/6.webp",
-  InlineDescription:
-    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt ratione soluta, accusantium nulla distinctio porro, impedit quidem eius dicta, doloribus aspernatur odio enim. Sed in quia nesciunt hic distinctio animi.",
-};
+import { projects } from "@/data/data";
 
 export default function project() {
+  const projectData = projects[0];
   return (
     <>
       <Head>
@@ -83,7 +44,7 @@ export default function project() {
         <InlineDescription inlineDescription={projectData.InlineDescription} />
         <ImageSlider images={projectData.sliderImages} />
         {/* <WideImage image={projectData.wideImage} /> */}
-        <InlineDescription inlineDescription={projectData.InlineDescription} />
+        {/* <InlineDescription inlineDescription={projectData.InlineDescription} /> */}
         <ImageSet imageSet={projectData.imageSet_2} />
         <Contact className={`mt-20`} />
       </LayoutWrapper>
