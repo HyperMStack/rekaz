8ad3481fbd73a840a6c89602cf1de469b2127f9c
MaenAlamin <ma3n.93@gmail.com>
2024-05-25 19:12:19 +0300

    updated sanity credentails and removed unnecessary folder from git

--- sanity/env.js
+++ sanity/env.js
@@ -1,7 +1,7 @@
 export const apiVersion =
   process.env.NEXT_PUBLIC_SANITY_API_VERSION || "2024-05-03";
 
-export const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET || "khmucuei";
+export const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET || "production";
 export const projectId =
-  process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || "production";
+  process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || "khmucuei";
 export const useCdn = false;
