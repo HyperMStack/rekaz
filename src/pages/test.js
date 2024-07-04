d4f053e0d330a3755456c4ec64f61c213eba73e4
MaenAlamin <ma3n.93@gmail.com>
2024-07-04 14:02:15 +0300

    removed testing trace

--- src/pages/test.js
+++ src/pages/test.js
@@ -1,21 +1,7 @@
 import ParallaxSlider from "@/components/index/ParallaxSlider";
 
 function test() {
-  const images = [
-    "/images/project2/1.webp",
-    "/images/project2/2.webp",
-    "/images/project2/3.webp",
-    "/images/project2/4.webp",
-    "/images/project2/1.webp",
-    "/images/project2/2.webp",
-  ];
-  // const testImages = [
-  //   "/test-images/01.jpg",
-  //   "/test-images/04.jpg",
-  //   "/test-images/02.jpg",
-  //   "/test-images/03.jpg",
-  // ];
-  return <ParallaxSlider images={images} />;
+  return <div>testing page</div>;
 }
 
 export default test;
