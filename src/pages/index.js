b9e7de342ab40425a4de786ad56405d0da34c454
MaenAlamin <ma3n.93@gmail.com>
2024-07-05 13:14:07 +0300

    sorted index page's sections

--- src/pages/index.js
+++ src/pages/index.js
@@ -164,9 +164,9 @@ export default function Home({
           language={websiteSettingsData.language}
         >
           <Hero data={heroSection} />
-          <ProjectsCarousel projects={projectPageData} />
           <Sectors sectorsData={sectorsSection} />
           <StatsIncrement statsData={statsSection} />
+          <ProjectsCarousel projects={projectPageData} />
           <Contact
             language={homePageData.language}
             contactsData={contactsData}
